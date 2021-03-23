03b0f6a340da0bea5bfa886bc348fe3cebc17e29
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-03-23 16:00:05 +0100

    adds script to calibrate lidar angles, it uses a room with a straight wall in front

--- calibrate_lidar_angles.py
+++ calibrate_lidar_angles.py
@@ -1,35 +1,72 @@
+import settings
+from src.env import VrepEnvironment
+from src.agents import Pioneer
 import numpy as np
 from matplotlib import pyplot as plt
+import time
+import math
+from scipy.signal import savgol_filter
 
 
-# x = np.loadtxt("x.txt")
-# y = np.loadtxt("y.txt")
+def get_and_save_data():
+    for scene, angle in zip(["0", "l90", "r90"], [(90, 180), (0, 90), (180, 270)]):
+        environment = VrepEnvironment(
+            settings.SCENES + '/room_calib_'+scene+'.ttt')
+        # environment.start_vrep()
+        environment.connect()
+        environment.load_scene(settings.SCENES + '/room_calib_'+scene+'.ttt')
+        environment.start_simulation()
+        agent = Pioneer(environment)
+        time.sleep(1)
 
-ranges = np.loadtxt("ranges.txt")
-# angles = np.linspace(-.75*np.pi, .75*np.pi, num=270)
+        sample = agent.read_lidars()
+        ranges = np.array(sample)[angle[0]:angle[1]]
+        for _ in range(0, 29):
+            sample = agent.read_lidars()
+            ranges += np.array(sample)[angle[0]:angle[1]]
+        ranges = ranges / 10
+        np.savetxt("ranges_"+scene+".txt", ranges)
 
-angles = np.arange(0, 270)
+        environment.stop_simulation()
+        environment.disconnect()
+        del environment
 
 
+# Idea do this in a loop maximizing hough transform data?
+def calib(r: np.ndarray) -> np.ndarray:
+    wall_normal = min(r)
+    wall_normal_idx = np.argmin(r)
+    angles = 180/math.pi * np.arccos(wall_normal/r)
 
-sin = np.sin(angles)
-cos = np.cos(angles)
+    midpoint = 44
+    angles[:wall_normal_idx] *= -1
+    angles += angles[midpoint]/2
+    return angles
 
-x = ranges*sin
-y = ranges*cos
-txt = np.arange(0, len(x))
 
-start = 77
-stop = 107
+def calib_and_save():
+    r = np.loadtxt("ranges_0.txt")
+    r[44] = (r[43]+r[45])/2
+    a_0 = calib(r)
+    a_l90 = calib(np.loadtxt("ranges_l90.txt"))
+    a_r90 = calib(np.loadtxt("ranges_r90.txt"))
 
-x = x[start:stop]
-y = y[start:stop]
-txt = txt[start:stop]
+    angles = np.zeros(270)
+    angles[90:180] = a_0
+    angles[0:90] = a_l90 - 90
+    angles[180:270] = a_r90 + 90
 
-fig, ax = plt.subplots()
-ax.scatter(x, y)
+    # smooth angles using savitzky golay
+    angles_s = savgol_filter(angles, 71, 3)
 
-for x, y, txt in zip(x, y, txt):
-    ax.annotate(txt, (x, y))
+    plt.plot(angles)
+    plt.plot(angles_s)
+    plt.show()
 
-plt.show()
+    np.savetxt("angles.txt", angles_s)
+
+
+if __name__ == "__main__":
+
+    # get_and_save_data()
+    calib_and_save()
